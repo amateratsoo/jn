89bf893bc471ef98cdb627157ddd9444460a2dcc
Jossani da Cruz <jossanidacruz2@gmail.com>
2025-04-18 13:20:05 +0100

    removed hero image alt text

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -10,7 +10,8 @@ export function Hero() {
         <img
           src='hero-image.jpg'
           // src='https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-          alt='uma imagem mostrando uma sala de estar bem arrumada'
+          // alt='uma imagem mostrando uma sala de estar bem arrumada'
+          alt=''
           className='rounded-2xl object-cover w-full h-full brightness-[30%]'
         />
 
